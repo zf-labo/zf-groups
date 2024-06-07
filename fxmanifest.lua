6652ccd55e8228713a3ef0f0d0f6206628140aa4
LeZach <contact.zgagne@gmail.com>
2024-06-07 00:32:42 -0400

    Fix FxManifest

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'LeZach & ZF Labo'
+description 'Group system for FiveM'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
+shared_script '@ox_lib/init.lua'
+files { 'bridge/*.lua', 'locales/*.json' }
